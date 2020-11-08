052f7f2fabc3b553a59100899948dbc83c6317ee
akinator001 <aakashgandhi1279@gmail.com>
2020-11-08 10:08:28 +0530

    [aakash] Check Employee Absent or Present

--- empWages.js
+++ empWages.js
@@ -1,1 +1,6 @@
-console.log("Welcome again to employee wages. ")
+const IS_ABSENT = 0 ;
+let empCheck = Math.floor(Math.random()* 10) % 2 ;
+if(empCheck == IS_ABSENT)
+    console.log("Employee is absent");
+else
+    console.log("Employee is present");
