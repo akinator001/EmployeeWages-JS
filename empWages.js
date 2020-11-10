3d1c41b16d7541f2e6991abd565a12c658167b41
akinator001 <aakashgandhi1279@gmail.com>
2020-11-10 16:07:39 +0530

    [aakash] Arrow Function is Used to calculate Total Wage

--- empWages.js
+++ empWages.js
@@ -1,82 +1,64 @@
-const IS_ABSENT = 0 ;
-const IS_PART_TIME = 1 ;
-const IS_FULL_TIME = 2 ;
-const PART_TIME_HOURS = 4 ;
-const FULL_TIME_HOURS = 8 ;
-const WAGE_PER_HOUR = 20 ;
+const IS_ABSENT = 0;
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
 const NUM_OF_WORKING_DAYS = 20;
 const MAX_HOURS_IN_MONTH = 160;
 
+let empDailyWageMap = new Map();
 
 function getWorkHours(empCheck){
     switch(empCheck){
-    case IS_PART_TIME:
-        return PART_TIME_HOURS ;
-    case IS_FULL_TIME:
-        return FULL_TIME_HOURS ;
-    default:
-        return 0;
-    }    
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
 }
 
 function calcDailyWage(empHrs){
-    return empHrs * WAGE_PER_HOUR;
+    return empHrs*WAGE_PER_HOUR;
 }
-
 let totalEmpHrs = 0;
 let totalWorkingDays = 0;
 let empDailyWageArr = new Array();
+
 while(totalEmpHrs<MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
-    empCheck = Math.floor(Math.random() * 10) % 3;
+    empCheck = Math.floor(Math.random()*10)%3;
     let empHrs = getWorkHours(empCheck);
     totalEmpHrs += empHrs;
     totalWorkingDays++;
     empDailyWageArr.push(calcDailyWage(empHrs));
+    empDailyWageMap.set(totalWorkingDays,calcDailyWage(empHrs));
 }
 
-let empWage = calcDailyWage(totalEmpHrs);
-console.log("Total days: "+totalWorkingDays+" Total hours: "+totalEmpHrs+" Emp Wage: "+empWage);
-
-// UC 7a
-let totalWage = 0;
-empDailyWageArr.forEach((dailyWage) => {totalWage += dailyWage});
-console.log("UC 7A, Total Emp Wage: "+totalWage);
+console.log(empDailyWageMap);
 function totalWages(totalWage,dailyWage){
     return totalWage+dailyWage;
 }
-console.log("UC7A - Emp Wage with reduce: "+empDailyWageArr.reduce(totalWages,0));
-//UC 7B
-let dailyCntr =0;
-function mapDayWithWage(dailyWage) {
-    dailyCntr++;
-    return dailyCntr+" = "+ dailyWage;
+
+console.log("Emp Wage Map total wage: "+Array.from(empDailyWageMap.values()).reduce(totalWages,0));
+
+const findTotal = (totalVal,dailyVal) => {
+    return totalVal+dailyVal;
 }
-let mapDayWithWageArr = empDailyWageArr.map(mapDayWithWage);
-console.log("UC7B - Daily Wage Map");
-console.log(mapDayWithWageArr);
+let count = 0;
+let totalHours = Array.from(empDailyWageMap.values()).reduce(findTotal,0);
+let totalSalary = empDailyWageArr.filter(dailyWage=>dailyWage>0).reduce(findTotal,0);
+console.log("UC9A - Emp Wage with Arrow: "+"Total Hours: "+totalEmpHrs+"Total Wages: "+ totalSalary);
 
-//UC 7C
-function fulltimeWage(dailyWage) {
-    return dailyWage.includes("160");
-}
-let fullDayWageArr = mapDayWithWageArr.filter(fulltimeWage);
-console.log(fullDayWageArr);
-
-//UC7D
-console.log("First time Fulltime wage was earned on: "+mapDayWithWageArr.find(fulltimeWage));
-
-//UC7E
-console.log("Check all element have full time wage "+ mapDayWithWageArr.every(fulltimeWage));
-
-//UC7F
-function isAnyPartTimeWage(dailyWage) {
-    return dailyWage.includes("80");
-}
-console.log("Check if any part time wage: "+mapDayWithWageArr.some(isAnyPartTimeWage));
-
-//UC7G
-function totalDaysWorked(numOfDays,dailyWage) {
-    if(dailyWage>0)return numOfDays+1;
-    return numOfDays;
-}
-console.log("Number of Days emp worked: "+ empDailyWageArr.reduce(totalDaysWorked,0));
+let nonWorkingDays = new Array();
+let partWorkingDays = new Array();
+let fullWorkingDays = new Array();
+empDailyWageMap.forEach((value,key,map)=>{
+    if(value == 160) fullWorkingDays.push(key);
+    else if(value == 80) partWorkingDays.push(key);
+    else nonWorkingDays.push(key);
+});
+console.log("Full Working Days: "+fullWorkingDays);
+console.log("Part Working Days: "+partWorkingDays);
+console.log("Non Working Days: "+nonWorkingDays);
